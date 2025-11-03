741489b7512b98d8d735ad90995078b648850c0c
varsh <varshinisiliveri22@gmail.com>
2025-11-03 22:27:23 +0530

    day1 Golang

---
+++ Go/Day1/code.go
@@ -0,0 +1,31 @@
+
+//  13. Day 1 Practice Tasks
+
+//  Create 3 separate .go files and try the following:
+
+//  Program 1: Print your name
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("My name is Varshini Siliveri.")
+}
+
+//  Program 2: Print today’s date and message
+// package main
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Date: 3 November 2025")
+//     fmt.Println("Day 1 of 100 Days of GoLang ")
+// }
+
+//  Program 3: Motivational Lines
+// package main
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Learning Go step by step!")
+//     fmt.Println("Consistency builds mastery.")
+//     fmt.Println("100 days, 100 lessons, 1 new skill ")
+// }
--- Javascript/01-Fundamentals-Part-1/starter/script.js
+++ Javascript/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,5 @@
 let js= 'amazing';
-    if (js==='amazing') alert('javascript is FUN!');
-    alert("hello world!");
+    // if (js==='amazing') alert('javascript is FUN!');
+    // alert("hello world!");
     console.log(1+34+22+54-29);
+    
